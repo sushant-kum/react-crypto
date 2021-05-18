1aa4278e27c8fe7affd471932c72e2701131e7dd
Sushant Kumar <sushant.kum96@gmail.com>
2021-05-18 20:14:55 +0530

    feat(darkmode): set darkmode as true by defualt

--- src/contexts/DarkMode.ts
+++ src/contexts/DarkMode.ts
@@ -2,7 +2,7 @@
  * @author Sushant Kumar
  * @email sushant.kum96@gmail.com
  * @create date Apr 19 2021 17:52:56 GMT+05:30
- * @modify date May 16 2021 21:16:09 GMT+05:30
+ * @modify date May 18 2021 20:00:21 GMT+05:30
  * @desc DarkModeContext
  */
 
@@ -11,7 +11,7 @@ import { Context, createContext } from "react";
 import { DarkModeContextValue } from "../models/DarkMode";
 
 export const darkModeContextInitialState: DarkModeContextValue = {
-  darkModeSelection: undefined,
+  darkModeSelection: true,
 };
 
 const DarkModeContext: Context<DarkModeContextValue> = createContext<DarkModeContextValue>(darkModeContextInitialState);
