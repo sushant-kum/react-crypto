a4136c0db32b09972e03b1f75dc6bb3602561a85
Sushant Kumar <sushant.kum96@gmail.com>
2021-08-17 18:36:03 +0530

    refactor(bundle-analyzer): prettified bundle-analyzer output

--- tools/bundle-analyzer/bundle-analyzer.tool.js
+++ tools/bundle-analyzer/bundle-analyzer.tool.js
@@ -9,24 +9,16 @@ const webpackConfigProd = require("react-scripts/config/webpack.config")("produc
 const webpack = require("webpack");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
-const green = (text) => {
-  return chalk.green.bold(text);
-};
-
-// pushing BundleAnalyzerPlugin to plugins array
 webpackConfigProd.plugins.push(new BundleAnalyzerPlugin());
 
-// optional - pushing progress-bar plugin for better feedback;
-// it can and will work without progress-bar,
-// but during build time you will not see any messages for 10-60 seconds (depends on the size of the project)
-// and decide that compilation is kind of hang up on you; progress bar shows nice progression of webpack compilation
 webpackConfigProd.plugins.push(
   new ProgressBarPlugin({
-    format: `${green("analyzing...")} ${green("[:bar]")}${green("[:percent]")}${green("[:elapsed seconds]")} - :msg`,
+    format: `${chalk.gray("[BA]")} ${chalk.green("[:percent]")} :bar ${chalk.green("[:elapsed seconds]")} - :msg`,
+    complete: "█",
+    incomplete: chalk.gray("█"),
   })
 );
 
-// actually running compilation and waiting for plugin to start explorer
 webpack(webpackConfigProd, (err, stats) => {
   if (err || stats.hasErrors()) {
     // eslint-disable-next-line no-console
